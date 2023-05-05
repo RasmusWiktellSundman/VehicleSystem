4e6c669fea0e9861d317464f29ca9b312bf64e52
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-05-05 22:15:05 +0200

    Added functionality to load all objects from one file and append to file

--- src/main/java/se/rmsit/VehicleSystem/FileHandler.java
+++ src/main/java/se/rmsit/VehicleSystem/FileHandler.java
@@ -32,6 +32,7 @@ public class FileHandler {
 		// Använder try-with-resource för att automatiskt stänga läsaren
 		try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file)))) {
 			writer.println("class: " + object.getClass().getName());
+			// Serialiserar och sparar det serialiserade objektet i filen
 			String serialized = object.serialize();
 			writer.write(serialized);
 		}
@@ -41,13 +42,40 @@ public class FileHandler {
 		storeObject(object, "");
 	}
 
+	public static void appendObjectToFile(Fetchable object, String fileName) throws IOException {
+		boolean fileExisted = true;
+		File file = new File(dataDirectoryPath+"\\"+fileName+".txt");
+		// Skapar ny fil ifall den inte redan finns
+		if(!file.exists()) {
+			file.createNewFile();
+			fileExisted = false;
+		}
+
+		// Använder try-with-resource för att automatiskt stänga läsaren
+		try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))) {
+			// Sparar klassen som används, sparas högst upp i filen då alla objekt är av samma typ
+			if(!fileExisted) {
+				writer.println("class: " + object.getClass().getName());
+			} else {
+				// Skriver objektseparerare till filen, endast mellan objekt, det vill säga inte direkt efter class:
+				writer.write("\n----\n");
+			}
+
+			// Serialiserar och sparar det serialiserade objektet i filen
+			String serialized = object.serialize();
+			writer.write(serialized);
+		}
+	}
+
 	public static Fetchable loadObject(Fetchable fetchable, String id, String subPath) throws IOException {
 		File file = new File(dataDirectoryPath+"\\"+subPath+"\\"+id+".txt");
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 			// Läser in data från filen och sparar i objektet
-			loadOneObject(fetchable, reader);
+			loadOneObjectFromReader(fetchable, reader);
 			return fetchable;
-		}
+		// Filen innehåller endast ett objekt, är därför irrelevant att filen nått slutet.
+		} catch (FileNotFoundException ignored) {}
+		return null;
 	}
 
 	public static Fetchable loadObject(Fetchable fetchable, String id) throws IOException {
@@ -64,25 +92,12 @@ public class FileHandler {
 	public static Fetchable loadObject(String id, String subPath) throws IOException {
 		File file = new File(dataDirectoryPath+"\\"+subPath+"\\"+id+".txt");
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-
-
-			// Skapar objekt utifrån klass i datafil
-			// Första raden ska följa formatet class: klass-namn
-			// Skapar nytt objekt från clazz
-			//TODO: Kolla att den innehåller class:
-			String className = reader.readLine().split(" ")[1];
-			Class clazz = Class.forName(className);
-			Constructor constructor = clazz.getConstructor();
-			Fetchable fetchable = (Fetchable) constructor.newInstance();
+			// Skapar tom klass som implementerar Fetchable. Klasstypen är given i filen.
+			Fetchable fetchable = createEmptyFetchable(reader);
 
 			// Läser in data från filen och sparar i objektet
-			loadOneObject(fetchable, reader);
+			loadOneObjectFromReader(fetchable, reader);
 			return fetchable;
-		} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Missing default constructor");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Invalid class in data-file, " + file.getPath());
 		} catch (FileNotFoundException e) {
 			return null;
 		}
@@ -109,36 +124,66 @@ public class FileHandler {
 		for (File file : directory.listFiles()) {
 			// Använder try-with-resource för att automatiskt stänga läsaren
 			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-				// Skapar objekt utifrån klass i datafil
-				// Första raden ska följa formatet class: klass-namn
-				// Skapar nytt objekt från clazz
-				//TODO: Kolla att den innehåller class:
-				String className = reader.readLine().split(" ")[1];
-				Class clazz = Class.forName(className);
-				Constructor constructor = clazz.getConstructor();
-				Fetchable fetchable = (Fetchable) constructor.newInstance();
+				// Skapar tom klass som implementerar Fetchable. Klasstypen är given i filen.
+				Fetchable fetchable = createEmptyFetchable(reader);
 
 				// Läser in data från filen och sparar i objektet
-				loadOneObject(fetchable, reader);
+				loadOneObjectFromReader(fetchable, reader);
 				fetchables.add(fetchable);
-			} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
-				e.printStackTrace();
-				throw new RuntimeException("Missing default constructor");
-			} catch (ClassNotFoundException e) {
-				throw new RuntimeException("Invalid class in data-file, " + file.getPath());
 			}
 		}
 
 		return fetchables;
 	}
 
-	private static void loadOneObject(Fetchable fetchable, BufferedReader reader) throws IOException {
+	public static List<Fetchable> getAllObjectsFromOneFile(String fileName) throws IOException {
+		File dataFile = new File(dataDirectoryPath+"\\"+fileName+".txt");
+		List<Fetchable> fetchables = new ArrayList<>();
+
+		// Kollar så filen finns, returnerar tom lista ifall den inte finns
+		if(!dataFile.exists())
+			return fetchables;
+
+		// Använder try-with-resource för att automatiskt stänga läsaren
+		try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
+			// TODO: Kolla om filen innehåller class:
+			// Hämtar klasstyp för alla klasser som ska laddas in
+			String className = reader.readLine().split("class: ")[1];
+
+			// Upprepar tills alla objekt har lästs in.
+			while(true) {
+				// Skapar tom klass som implementerar Fetchable.
+				Fetchable fetchable = createEmptyFetchable(className);
+
+				// Läser in data från filen och sparar i objektet, metoden läser endast in till nästa ----, det vill säga ett objekt
+				// Metoden returnerar false när alla objekt är inlästa
+				if(!loadOneObjectFromReader(fetchable, reader)) {
+					fetchables.add(fetchable);
+					break;
+				}
+				fetchables.add(fetchable);
+			}
+		}
+
+		return fetchables;
+	}
+
+	/**
+	 *
+	 * @param fetchable
+	 * @param reader Läsaren att hämta data ifrån
+	 * @return True ifall filen innehåller fler objekt, annars false
+	 * @throws IOException
+	 */
+	private static boolean loadOneObjectFromReader(Fetchable fetchable, BufferedReader reader) throws IOException {
 		// Skapar objekt från reader data (samma som store metoden)
 		while (true) {
 			String line = reader.readLine();
 			// Om filen innehåller flera objekt separeras de av "----"
-			if(line == null || line.equals("----")) {
-				break;
+			if(line == null) {
+				return false;
+			} else if(line.equals("----")) {
+				return true;
 			}
 			// Delar upp raden i nyckel-data par
 			String[] tokens = line.split(": ");
@@ -147,6 +192,35 @@ public class FileHandler {
 				data = null;
 			}
 			fetchable.loadData(tokens[0], data);
+		}
+	}
+
+	/**
+	 * Skapar ett Fetchable objekt med den specifika klassen som står på första raden reader läser in.
+	 * Klassens standardkonstruktor används för att skapa objektet
+	 * @param reader
+	 * @return En klass som implementerar Fetchable
+	 * @throws IOException
+	 */
+	private static Fetchable createEmptyFetchable(BufferedReader reader) throws IOException {
+		//TODO: Kolla att den innehåller class:
+		String line = reader.readLine();
+		if(line == null) {
+			return null;
+		}
+		String className = line.split(" ")[1];
+		return createEmptyFetchable(className);
+	}
+
+	private static Fetchable createEmptyFetchable(String className) throws IOException {
+		try {
+			Constructor constructor = Class.forName(className).getConstructor();
+			return (Fetchable) constructor.newInstance();
+		} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Missing default constructor");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Invalid class in data-file");
 		}
 	}
 }
