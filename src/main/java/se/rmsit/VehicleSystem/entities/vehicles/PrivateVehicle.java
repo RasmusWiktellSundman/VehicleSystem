3dbf3b2a1236872099b2e1ec673bea28f7d0467c
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-05-05 22:15:09 +0200

    Added saving of car address

--- src/main/java/se/rmsit/VehicleSystem/entities/vehicles/PrivateVehicle.java
+++ src/main/java/se/rmsit/VehicleSystem/entities/vehicles/PrivateVehicle.java
@@ -3,6 +3,7 @@ package se.rmsit.VehicleSystem.entities.vehicles;
 import se.rmsit.VehicleSystem.entities.Customer;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public abstract class PrivateVehicle extends Vehicle {
 	private String address;
@@ -31,5 +32,36 @@ public abstract class PrivateVehicle extends Vehicle {
 			throw new IllegalArgumentException("Address cant be null");
 		}
 		this.address = address;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+
+		PrivateVehicle that = (PrivateVehicle) o;
+
+		return Objects.equals(address, that.address);
+	}
+
+	@Override
+	public String serialize() {
+		String serialized = super.serialize() + "\n";
+		serialized += "address: " + getAddress() + "\n";
+		return serialized;
+	}
+
+	/**
+	 * Laddar in ett nyckel-värde par till objektet
+	 * @param key Nyckeln för värdet
+	 * @param value Värdet
+	 */
+	@Override
+	public void loadData(String key, String value) {
+		super.loadData(key, value);
+		if ("address".equals(key)) {
+			setAddress(value);
+		}
 	}
 }
--- src/test/java/se/rmsit/VehicleSystem/entities/vehicles/CarTest.java
+++ src/test/java/se/rmsit/VehicleSystem/entities/vehicles/CarTest.java
@@ -2,11 +2,14 @@ package se.rmsit.VehicleSystem.entities.vehicles;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import se.rmsit.VehicleSystem.TestVehicle;
 import se.rmsit.VehicleSystem.entities.Customer;
 
+import java.io.IOException;
 import java.util.Calendar;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
 	private Car car;
@@ -22,6 +25,18 @@ class CarTest {
 		Calendar today = Calendar.getInstance();
 		assertThrows(IllegalArgumentException.class, () -> new Car(new Customer(), "ABC123", -1, 4, today, (Calendar) today.clone(), 10, "something"));
 		assertThrows(IllegalArgumentException.class, () -> car.setMaximumPassengers(-1));
+	}
+
+	@Test
+	void canGetFromStorage() throws IOException {
+		Customer testCustomer = new Customer("1", "Test", "Dev", null, null, null, null, false, "test@testing.se", "hej");
+		testCustomer.save();
+		Vehicle vehicle = new Car(testCustomer, "ABC123", 4, 4, Calendar.getInstance(), Calendar.getInstance(), 100, "an address");
+		Vehicle vehicle2 = new Car(testCustomer, "ABC124", 1, 2, Calendar.getInstance(), Calendar.getInstance(), 100, "an address2");
+		assertDoesNotThrow(vehicle::save);
+		assertDoesNotThrow(vehicle2::save);
+		assertEquals(vehicle, Vehicle.getByRegistrationNumber("ABC123"));
+		assertEquals(vehicle2, Vehicle.getByRegistrationNumber("ABC124"));
 	}
 
 	@Test
